c8389f32e477f42c9d6d70e818deb299a85d3891
Ubuntu <ubuntu@ip-172-31-31-83.us-east-2.compute.internal>
2021-01-14 00:38:08 +0000

    Archivo ignorados servidor

--- resources/views/web/default/user/layout/layout.blade.php
+++
@@ -1,9 +0,0 @@
-@include(getTemplate().'.view.layout.header',['title'=>'User Panel'])
-@if($user['vendor'] == 1)
-    @include(getTemplate().'.user.layout.menu')
-@else
-    @include(getTemplate().'.user.layout_user.menu')
-@endif
-@yield('pages')
-@include(getTemplate().'.user.layout.modals')
-@include(getTemplate().'.view.layout.footer')
